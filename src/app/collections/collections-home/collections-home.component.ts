3c0fe0d90d09d91ba8004a7e5a54f27a51da8ecf
SJCMN <sam.clavette@gmail.com>
2022-02-23 15:54:31 -0600

    building table component

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -3,13 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-collections-home',
   templateUrl: './collections-home.component.html',
-  styleUrls: ['./collections-home.component.css']
+  styleUrls: ['./collections-home.component.css'],
 })
 export class CollectionsHomeComponent implements OnInit {
+  data: Array<object> = [
+    { name: 'James', age: 34, job: 'Chump', employed: true },
+    { name: 'Jeremiah', age: 44, job: 'Dir of Chumps', employed: true },
+    { name: 'Luis', age: 25, job: 'Design Chump', employed: true },
+    { name: 'Drew', age: 24, job: 'Design Chump', employed: false }
+  ];
 
-  constructor() { }
+  headers: Array<object> = [
+    { key: 'name', label: 'Name' },
+    { key: 'age', label: 'Age' },
+    { key: 'job', label: 'Job' },
+    { key: 'employed', label: 'Employed' },
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
