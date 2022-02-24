fd9fa4013ff0b86480b962e0a899a9cd16282751
SJCMN <sam.clavette@gmail.com>
2022-02-24 16:51:33 -0600

    :host:not(:first-of-type) finds second instance of divider to add margin-top 20px!

--- src/app/views/items/items.component.ts
+++ src/app/views/items/items.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-items',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./items.component.css']
 })
 export class ItemsComponent implements OnInit {
+
+  @Input() items: Array<any> = [];
 
   constructor() { }
 
--- src/app/views/views-home/views-home.component.ts
+++ src/app/views/views-home/views-home.component.ts
@@ -13,6 +13,12 @@ export class ViewsHomeComponent implements OnInit {
     { value: 50, label: 'Reviews'}
   ];
 
+  items: Array<object> = [
+    { title: 'Green Sofa', description: 'Look at this green velvet!', url: 'https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y291Y2h8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60' },
+    { title: 'Leather Sofa', description: 'Love this camel color leather!', url: 'https://images.unsplash.com/photo-1540574163026-643ea20ade25?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y291Y2h8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'},
+    { title: 'Chesterfield', description: 'OMG need a Chesterfield!!!', url: 'https://images.unsplash.com/photo-1573866926487-a1865558a9cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNvdWNofGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'}
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
